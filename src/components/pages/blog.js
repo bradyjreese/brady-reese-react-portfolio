de4356c691e20bee7bab22c87d10f7e344696a70
northerreese <bradyj.reese@gmail.com>
2020-08-24 16:54:32 -0600

    updated blog features and styles

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import axios from 'axios';
 import BlogItem from "../blog/blog-item";
+import BlogModal from '../modals/blog-modal';
 
 export default class Blog extends Component {
     constructor() {
@@ -12,12 +13,35 @@ export default class Blog extends Component {
             blogItems: [],
             totalCount: 0,
             currentPage: 0,
-            isLoading: true
+            isLoading: true,
+            blogModalIsOpen: false
         }
 
         this.getBlogItems = this.getBlogItems.bind(this);
         this.onScroll= this.onScroll.bind(this);
         window.addEventListener("scroll", this.onScroll, false);
+        this.handleNewBlogClick = this.handleNewBlogClick.bind(this);
+        this.handleModalClose = this.handleModalClose.bind(this);
+        this.handleSuccessfulNewBlogSubmission = this.handleSuccessfulNewBlogSubmission.bind(this);
+    }
+
+    handleSuccessfulNewBlogSubmission(blog) {
+        this.setState({
+            blogModalIsOpen: false,
+            blogItems: [blog].concat(this.state.blogItems)
+        })
+    }
+
+    handleModalClose() {
+        this.setState({
+            blogModalIsOpen: false
+        });
+    }
+
+    handleNewBlogClick() {
+        this.setState({
+            blogModalIsOpen: true
+        });
     }
 
     onScroll() {
@@ -40,7 +64,6 @@ export default class Blog extends Component {
             `https://bradyjreese.devcamp.space/portfolio/portfolio_blogs?page=${this.state.currentPage}`,
             { withCredentials: true }
         ).then(response => {
-            console.log("getting blog items", response.data);
             this.setState({
                 blogItems: this.state.blogItems.concat(response.data.portfolio_blogs),
                 totalCount: response.data.meta.total_records,
@@ -51,7 +74,7 @@ export default class Blog extends Component {
         })
     }
 
-    // Update from componentWillMount(), because it's being deprecated.
+    // Updated from componentWillMount(), because it's being deprecated.
     componentDidMount() {
         this.getBlogItems();
     }
@@ -67,6 +90,20 @@ export default class Blog extends Component {
 
         return (
             <div className="blog-container">
+                <BlogModal 
+                    handleSuccessfulNewBlogSubmission={this.handleSuccessfulNewBlogSubmission}
+                    handleModalClose={this.handleModalClose}
+                    modalIsOpen={this.state.blogModalIsOpen}
+                />
+
+                {this.props.loggedInStatus === "LOGGED_IN" ?
+                    <div className="new-blog-link">
+                        <a onClick={this.handleNewBlogClick}>
+                            <FontAwesomeIcon icon="plus-square" />
+                        </a>
+                    </div> : null
+                }
+
                 <div className="content-container">{blogRecords}</div>
 
                 {this.state.isLoading ? (
