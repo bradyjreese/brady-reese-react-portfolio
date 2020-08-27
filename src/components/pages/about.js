7f4b8e5e5269d8d481f6dd03031a21fc51782b09
northerreese <bradyj.reese@gmail.com>
2020-08-27 16:51:08 -0600

    finalized portfolio demo site

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import profilePicture from "../../../static/assets/images/bio/brady.jpg"
 
 export default function() {
     return (
-        <div>
-            About
+        <div className="content-page-wrapper">
+            <div
+                className="left-column"
+                style={{
+                    background: "url(" + profilePicture + ") no-repeat",
+                    backgroundSize: "cover",
+                    backgroundPosition: "center"
+                }}
+            />
+
+            <div className="right-column">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. In doloremque id quas quos est aspernatur repellendus modi esse, voluptas explicabo! Hic iste, eligendi est qui sapiente nemo explicabo in dolorem?
+                Quaerat sequi minus nesciunt praesentium magnam laudantium nam ducimus similique, veritatis consectetur ipsam, nemo possimus quo. Eaque totam eos, tempora eius excepturi obcaecati aliquam perspiciatis commodi est cupiditate! Sed, labore?
+                Iure, corrupti enim quos, itaque atque laudantium laborum minus perferendis odit ut ducimus? Est distinctio cumque, numquam cum fugiat rem? Itaque veniam consequuntur laboriosam quos distinctio consectetur autem? A, omnis.
+                Error suscipit sequi, rerum voluptate mollitia ab voluptas voluptatibus, dolorem deleniti vero exercitationem fugit nostrum optio! Minus, dolorem perferendis expedita quis ipsam asperiores quisquam nulla omnis in rem, numquam sed?
+                Earum laboriosam esse nesciunt maxime amet exercitationem voluptate beatae cumque repellat nam! Optio, porro. Aspernatur nam deleniti error dolorum! Corporis omnis deserunt nam odit sequi ipsa a hic dignissimos pariatur!
+                Optio modi sapiente dolore obcaecati qui cum corrupti repellat aperiam unde! Venia
+            </div>
         </div>
     );
 }
--- src/helpers/icons.js
+++ src/helpers/icons.js
@@ -1,8 +1,28 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faTrash, faSignOutAlt, faEdit, faSpinner, faPlusSquare } from "@fortawesome/free-solid-svg-icons";
+import {
+    faTrash,
+    faSignOutAlt,
+    faEdit,
+    faSpinner,
+    faPlusSquare,
+    faPhone,
+    faEnvelope,
+    faMapMarkedAlt,
+    faLock
+} from "@fortawesome/free-solid-svg-icons";
 
 const Icons = () => {
-    return library.add(faTrash, faSignOutAlt, faEdit, faSpinner, faPlusSquare)
+    return library.add(
+        faTrash,
+        faSignOutAlt,
+        faEdit,
+        faSpinner,
+        faPlusSquare,
+        faPhone,
+        faEnvelope,
+        faMapMarkedAlt,
+        faLock
+    )
 };
 
 export default Icons;
